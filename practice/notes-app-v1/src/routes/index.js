57b98c8238c773737c41b74f2a321348e0765ce9
andy.kristanto <aktdev.id@gmail.com>
2026-02-01 12:11:53 +0700

    feat: [add user & get user by id] create new endpoint

--- practice/notes-app-v1/src/routes/index.js
+++ practice/notes-app-v1/src/routes/index.js
@@ -14,3 +14,14 @@
 // router.delete('/notes/:id', deleteNoteById);
 
 // export default router;
+
+import { Router } from 'express';
+import notes from '../services/notes/routes/index.js';
+import users from '../services/users/routes/index.js';
+
+const router = Router();
+
+router.use('/', notes);
+router.use('/', users);
+
+export default router;
---
+++ practice/notes-app-v1/src/services/users/repositories/user-repositories.js
@@ -0,0 +1,46 @@
+import { Pool } from 'pg';
+import { nanoid } from 'nanoid';
+import bcrypt from 'bcrypt';
+
+class UserRepositories {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async createUser({ username, password, fullname }) {
+    const id = nanoid(16);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const createdAt = new  Date().toISOString();
+    const updateAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, username, hashedPassword, fullname, createdAt, updateAt],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  };
+
+  async verifyNewUserName(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rowCount > 0;
+  };
+
+  async getUserById(id) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+}
+
+export default new UserRepositories();
