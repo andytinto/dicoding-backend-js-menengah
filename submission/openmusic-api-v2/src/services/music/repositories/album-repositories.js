1e173a37e9ef36e29a54da7b4e121d86ca6b64b9
andy.kristanto <aktdev.id@gmail.com>
2026-02-05 15:52:46 +0700

    feat: create project open music v2

---
+++ submission/openmusic-api-v2/src/services/music/repositories/album-repositories.js
@@ -0,0 +1,74 @@
+import { Pool } from 'pg';
+import { nanoid } from 'nanoid';
+class AlbumRepositories {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async createAlbums({ name, year }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+    const query = {
+      text: 'INSERT INTO albums(id, name, year, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING id, name, year, created_at, updated_at',
+      values: [id, name, year, createdAt, updatedAt],
+    };
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+
+  async getAlbumByIdWithSongs(id) {
+    const query = {
+      text: `
+      SELECT 
+        a.id AS album_id,
+        a.name,
+        a.year,
+        s.id AS song_id,
+        s.title,
+        s.performer
+      FROM albums a
+      LEFT JOIN songs s ON s.album_id = a.id
+      WHERE a.id = $1`,
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      return null;
+    }
+
+    return result.rows;
+  }
+
+  async updateAlbumById({ id, name, year }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id, name, year, created_at, updated_at',
+      values: [name, year, updatedAt, id],
+    };
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1',
+      values: [id],
+    };
+    await this._pool.query(query);
+  }
+
+}
+
+export default new AlbumRepositories();
---
+++ submission/openmusic-api-v2/src/services/music/validator/schema.js
@@ -0,0 +1,33 @@
+import Joi from 'joi';
+
+export const createAlbumPayload = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.number().integer().positive().required(),
+});
+
+export const updateAlbumPayload = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.number().integer().positive().required(),
+});
+
+export const createSongsPayload = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().integer().positive().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number()
+    .integer()
+    .positive()
+    .allow(null),
+  albumId: Joi.string()
+    .allow(null),
+});
+
+export const updateSongsPayload = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().integer().positive().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number().integer().positive().allow(null),
+  albumId: Joi.string().allow(null)
+});
