4b7c7d390ac126a378e2375bada7a7de02a5e679
andy.kristanto <aktdev.id@gmail.com>
2026-02-13 16:07:18 +0700

    feat: export notes implemented

--- practice/notes-app-v3/src/routes/index.js
+++ practice/notes-app-v3/src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import notes from '../services/notes/routes/index.js';
 import users from '../services/users/routes/index.js';
 import authentications from '../services/authentications/routes/index.js';
+import exports from '../services/exports/routes/index.js';
 
 const router = Router();
 
 router.use('/', notes);
 router.use('/', users);
 router.use('/', authentications);
+router.use('/', exports);
 
 export default router;
